ac336079246ba817c4b1416a7a3aa00357b3ced3
Alok kumar paul <alokkumarpaul22076@gmail.com>
2026-06-15 10:41:43 +0600

    add whitenoise

--- poterry/settings.py
+++ poterry/settings.py
@@ -68,6 +68,7 @@ CACHES = {   #memory te cache rakhbe, database use korle beshi load porbe
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -151,6 +152,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
