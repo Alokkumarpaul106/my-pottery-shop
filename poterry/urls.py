8ac7b042d33e458b6b3b1c9dcf541930409ee1ee
Alok kumar paul <alokkumarpaul22076@gmail.com>
2026-06-19 18:28:16 +0600

    Enhance URL patterns for media file serving
    Added static file serving for media files in DEBUG mode.

--- poterry/urls.py
+++ poterry/urls.py
@@ -18,10 +18,14 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve # এই লাইনটি যোগ করুন
+from django.urls import re_path # এই লাইনটি যোগ করুন
 urlpatterns = [
     path('pronam_babaSongkor2026admin/', admin.site.urls),
     path('',include('backend.urls')),
     path('accounts/', include('allauth.urls')),  # Google login er jonno
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# DEBUG = False থাকলেও এটি মিডিয়া ফাইল সার্ভ করবে
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
