bb488ddc707045b70961b4d4a0605b3719c0eb28
Alok kumar paul <alokkumarpaul22076@gmail.com>
2026-09-14 11:56:50 +0600

    Modify STATICFILES_STORAGE and add STORAGES settings
    Updated STATICFILES_STORAGE configuration and added STORAGES settings for file and static file storage.

--- poterry/settings.py
+++ poterry/settings.py
@@ -152,7 +152,16 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  ata django purono version er jonno chilo
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
